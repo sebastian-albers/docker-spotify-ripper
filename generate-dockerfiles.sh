e54c71082a902365de7fbb8f548225968127ad19
Sebastian Albers <seb.albers@gmail.com>
2020-03-05 22:32:23 +0100

    Removed arm64v8 architecture.

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -2,11 +2,10 @@
 
 set -eou pipefail
 
-for docker_arch in amd64 arm32v7 arm64v8; do
+for docker_arch in amd64 arm32v7; do
   case ${docker_arch} in
     amd64   ) qemu_arch="x86_64" ;;
     arm32v7 ) qemu_arch="arm" ;;
-    arm64v8 ) qemu_arch="aarch64" ;;
   esac
   cp Dockerfile.cross Dockerfile.${docker_arch}
   sed -i"" "s|__BASEIMAGE_ARCH__|${docker_arch}|g" Dockerfile.${docker_arch}
