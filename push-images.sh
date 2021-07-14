472fef7981dda86b0c5a351015af731469eb545b
Sebastian Albers <seb.albers@gmail.com>
2021-07-14 09:02:41 +0200

    Removed references to arm64v8 images.

--- push-images.sh
+++ push-images.sh
@@ -5,10 +5,9 @@ set -euxo pipefail
 # login to Docker Hub
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-# push latest amd64, arm32v7 and arm64v8 images
+# push latest amd64 and arm32v7 images
 docker push "sebastianalbers/spotify-ripper:latest-amd64"
 docker push "sebastianalbers/spotify-ripper:latest-arm32v7"
-docker push "sebastianalbers/spotify-ripper:latest-arm64v8"
 
 # read version from Docker container
 version=$(./read-version.sh amd64)
